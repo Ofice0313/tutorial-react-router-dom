8759cf5f13168734d358bae48671c4fb94b624d0
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-03-05 17:55:37 +0200

    Links without full page reload

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,11 +1,22 @@
 
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>BookKeeper</h1>
+      <nav 
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}>
+
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/dashboard">Dashboard</Link>
+
+      </nav>
     </>
   )
 }
